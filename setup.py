32ccdf12816fb742c67438fb27458c406cd24dba
mattdoug604 <mattdoug604@gmail.com>
2020-12-06 11:21:46 -0800

    add sample database and text

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import find_packages, setup
 
 setup(
     name="rolltables",
-    version="0.1.0",
+    version="0.1.1",
     author="Matt Douglas",
     author_email="mattdoug604@gmail.com",
     packages=find_packages(),
     description="A flexible Python package for creating and using random tables with your favorite TTRPG.",
     python_requires=">=3.6",
     install_requires=[],
+    include_package_data=True,
     extras_require={"dev": ["black", "isort", "mypy"], "tests": ["pytest", "pytest-mock"]},
     entry_points={"console_scripts": ["rolltables=rolltables.cli:main"]},
 )
