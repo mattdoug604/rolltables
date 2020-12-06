12644009e6d6355989f14a32ae5de57cd488e974
mattdoug604 <mattdoug604@gmail.com>
2020-12-06 11:16:47 -0800

    remove extra print statements

--- rolltables/cli.py
+++ rolltables/cli.py
@@ -10,9 +10,6 @@ def main():
     except IndexError:
         raise ValueError(f"Usage: {__name__} <file_to_format> <database_file>")
 
-    print(file_to_format)
-    print(database_file)
-
     database = load(database_file)
     with open(file_to_format, "r") as fh:
         format_string = fh.read()
